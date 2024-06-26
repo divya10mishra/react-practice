67475069add51c39fa646308cc43c2a379af778e
divya_mishra <divyamishra075@gmail.com>
2024-06-27 00:05:37 +0530

    concepts

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import "./App.css";
 import { createBrowserRouter, Outlet } from "react-router-dom";
 import Header from "./features/Header/Header"
-import Counter from './features/counter/Counter'
 import UseMemoEx from './concepts/useMemo/index'
 import UseCallbackEx from './concepts/useCallback/parent';
 import ComponentA from "./concepts/StateUplift/ComponentA";
 import ClassComponentA from "./concepts/StateUpliftClassComponent/ComponentA";
-import Pagination from "./features/Pagination/Pagination.js";
 import UseRef from './concepts/UseRef'
 
 function App() {
@@ -23,16 +21,7 @@ export const appRouter = createBrowserRouter([
     path: "/",
     element: <App/>,
     children: [
-      {
-        path:'/pagination',
-        element:<Pagination/>,
-        errorElement:<>Error fallback page</>
-      },
-      {
-        path: '/counter',
-        element: <Counter/>,
-        errorElement:<>Error fallback page</>
-      },
+   
       {
         path:'/usememo',
         element: <UseMemoEx/>
