751825c00a7c8d2345f6fbf45879ec94cf8bd880
divya_mishra <divyamishra075@gmail.com>
2024-06-30 19:33:50 +0530

    authorization-jwt

--- server/index.js
+++ server/index.js
@@ -42,26 +42,37 @@ app.post("/registration", async (req, res) => {
     console.error(err);
   }
 });
-
+app.get("/posts", authenticateToken, async (req, res) => {
+  try {
+    const payload = req.authUser.username;
+    console.log(payload, "before");
+    const dbUser = await User.findOne({ username: payload });
+    console.log(dbUser, "payload");
+    if (dbUser) {
+      res.status(200).json({ email: dbUser.email });
+    } else {
+      res.status(200).json({ message: "email not found" });
+    }
+  } catch (e) {
+    res.status(e.status).json({ message: "posts not send" });
+  }
+});
 app.post("/login", async (req, res) => {
   try {
-    const data = req.body;
-    //const authUser = { name: data.username };
-    //const accessToken = jwt.sign(authUser, process.env.ACCESS_TOKEN_SECRET);
-    // res.json({ accessToken: accessToken });
-    // console.log(accessToken, process.env.ACCESS_TOKEN_SECRET, authUser);
-    let dbUser = await User.findOne({ username: data.username });
+    const { username, password } = req.body;
+    let dbUser = await User.findOne({ username: username });
     if (!dbUser) {
       const error = new Error("User is not registered");
       error.status = 403;
       throw error;
     }
-    // let decrypt = await bcrypt.compare(data.password, dbUser.password);
+    let decrypt = await bcrypt.compare(password, dbUser.password);
     if (dbUser.username && decrypt) {
-      //if (dbUser.username) {
+      const authUser = { username };
+      const accessToken = jwt.sign(authUser, process.env.ACCESS_TOKEN_SECRET);
+      console.log(accessToken, process.env.ACCESS_TOKEN_SECRET, authUser);
       res.status(200).json({
-        email: dbUser.email,
-        // accessToken: accessToken
+        accessToken: accessToken,
       });
     } else {
       res.status(200).json({ message: "Not verified!" });
@@ -71,17 +82,17 @@ app.post("/login", async (req, res) => {
   }
 });
 
-// function authenticateToken(req, res, next) {
-//   const authHeader = req.header["authorization"];
-//   const token = authHeader && authHeader.split(" ")[1];
-//   if (token == null) return res.status(401);
+function authenticateToken(req, res, next) {
+  const authHeader = req.headers["authorization"];
+  const token = authHeader && authHeader.split(" ")[1];
+  if (token == null) return res.status(401);
 
-//   jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
-//     if (err) return res.status(403);
-//     req.authUser = user;
-//     next();
-//   });
-// }
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
+    if (err) return res.status(403);
+    req.authUser = user;
+    next();
+  });
+}
 
 app.listen(5000, () => {
   console.log("app is running");
